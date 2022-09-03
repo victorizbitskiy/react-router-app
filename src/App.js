d99897618c5b7e98c81b11a0c7f3b8fca6395bf8
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-03 20:12:53 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { About, Contact, Events, Home, Products } from './pages';
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export function Home() {
+  return (
+    <div>
+      <h1>[Company Website]</h1>
+      <nav>
+        <Link to="about">About</Link>
+        <Link to="events">Events</Link>
+        <Link to="products">Products</Link>
+        <Link to="contact">Contact</Link>
+      </nav>
+    </div>
+  );
+}
+
+export function About() {
+  return (
+    <div>
+      <h1>[About]</h1>
+    </div>
+  );
+}
+
+export function Events() {
+  return (
+    <div>
+      <h1>[Events]</h1>
+    </div>
+  );
+}
+
+export function Products() {
+  return (
+    <div>
+      <h1>[Products]</h1>
+    </div>
+  );
+}
+
+export function Contact() {
+  return (
+    <div>
+      <h1>[Contact]</h1>
+    </div>
+  );
+}
