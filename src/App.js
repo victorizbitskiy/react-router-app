256b26a04341fd23569a08c762c345b5ed4ea883
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-05 20:15:36 +0300

    nested routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { About, Contact, Events, Home, Products } from './pages';
+import { About, Contact, Events, Home, Products, Services } from './pages';
 import { Whoops404 } from './utils/Whoops404';
 
 
@@ -9,7 +9,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />} >
+          <Route path='services' element={<Services />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
--- src/pages.jsx
+++ src/pages.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function Home() {
   return (
@@ -19,7 +19,26 @@ export function About() {
   return (
     <div>
       <h1>[About]</h1>
+      <Outlet />
     </div>
+  );
+}
+
+export function Services() {
+  return (
+    <section>
+      <h2>Our Services</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit
+        anim id est laborum.
+      </p>
+    </section>
   );
 }
 
