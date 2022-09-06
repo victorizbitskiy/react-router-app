48e608a1feabf1fa09cca746bbeb3f4a17d75f0a
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-09-06 17:31:15 +0300

    redirect/navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Redirect, Navigate } from 'react-router-dom';
 import { About, Contact, Events, Home, Products, Services } from './pages';
 import { Whoops404 } from './utils/Whoops404';
 
@@ -12,6 +12,7 @@ function App() {
         <Route path="/about" element={<About />} >
           <Route path='services' element={<Services />} />
         </Route>
+        <Route path="services" element={<Navigate to="/about/services" />} />
         <Route path="/events" element={<Events />} />
         <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} />
